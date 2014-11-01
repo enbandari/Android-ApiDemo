a1815bb778f57a6763fc93a1cf51d1cbb1af6770
enbandari <305779913@qq.com>
2014-11-01 12:22:47 +0800

    BouncingBalls源码解读：属性动画的运用

--- ApiDemos/src/com/example/android/apis/animation/BouncingBalls.java
+++ ApiDemos/src/com/example/android/apis/animation/BouncingBalls.java
@@ -18,10 +18,14 @@ package com.example.android.apis.animation;
 
 // Need the following import to get access to the app resources, since this
 // class is in a sub-package.
-import android.graphics.drawable.ColorDrawable;
-import com.example.android.apis.R;
+import java.util.ArrayList;
 
-import android.animation.*;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
@@ -37,19 +41,26 @@ import android.view.animation.AccelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.LinearLayout;
 
-import java.util.ArrayList;
+import com.example.android.apis.R;
 
-
+/*
+ * 关键点：属性动画ValueAnimator的运用。
+ */
 public class BouncingBalls extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //布局文件就是一个线性布局，如果你运行过程序，肯定一眼便知这界面肯定是动态画出来的。
         setContentView(R.layout.bouncing_balls);
         LinearLayout container = (LinearLayout) findViewById(R.id.container);
+        // 按F3看看MyAnimationView
         container.addView(new MyAnimationView(this));
     }
 
+    /*
+     * 运行程序之后，我们发现程序其实做了两件事，背景过渡动画，以及BouncingBalls
+     */
     public class MyAnimationView extends View {
 
         private static final int RED = 0xffFF8080;
@@ -67,6 +78,10 @@ public class BouncingBalls extends Activity {
             // Note that setting the background color will automatically invalidate the
             // view, so that the animated color, and the bouncing balls, get redisplayed on
             // every frame of the animation.
+            //下面这一块代码就是操作背景颜色变化的，注意属性动画的使用，如果我没记错的话，api14之前需要使用http://nineoldandroids.com/
+            //实际上这里用到了一点小技巧，ofInt方法传入的propertyName，如果大家对JavaBean比较熟悉的话，肯定马上能想到这里用反射机制操作backgroundColor的。
+            //View当中实际上是没有backgroundColor这个属性的，但却有setBackGroundColor的方法，由于背景可能是图片，也可能是颜色（颜色最终也是drawable），没有这个属性当然也是容易理解的。
+            //请大家注意，由于backgroundColor本质上是个方法，并不是一个属性，所以View并没有getBackGroundColor方法。在低版本api使用nineoldandroids的ValueAnimator时主要使用holder的方法，详见博客文章解析
             ValueAnimator colorAnim = ObjectAnimator.ofInt(this, "backgroundColor", RED, BLUE);
             colorAnim.setDuration(3000);
             colorAnim.setEvaluator(new ArgbEvaluator());
@@ -75,16 +90,23 @@ public class BouncingBalls extends Activity {
             colorAnim.start();
         }
 
+        /*
+         * 通过操作，我们发现每次调用下面的方法，会完成以下操作：
+         * 生成一个ball，动画运动到屏幕底，形变，恢复，再动画运动到初始位置，淡出。
+         */
         @Override
         public boolean onTouchEvent(MotionEvent event) {
+        	//只处理按下，和移动手指触点的操作，常见的还有ACTION_CANCEL ACTION_UP，这里都不处理
             if (event.getAction() != MotionEvent.ACTION_DOWN &&
                     event.getAction() != MotionEvent.ACTION_MOVE) {
                 return false;
             }
+            //初始化一个ball，告诉他出现的位置
             ShapeHolder newBall = addBall(event.getX(), event.getY());
 
             // Bouncing animation with squash and stretch
             float startY = newBall.getY();
+            //50f是小球的半径哈，这是屏幕底部的坐标
             float endY = getHeight() - 50f;
             float h = (float)getHeight();
             float eventY = event.getY();
@@ -92,24 +114,32 @@ public class BouncingBalls extends Activity {
             ValueAnimator bounceAnim = ObjectAnimator.ofFloat(newBall, "y", startY, endY);
             bounceAnim.setDuration(duration);
             bounceAnim.setInterpolator(new AccelerateInterpolator());
+            
+            //如果不做这个x方向的动画，你就会发现下面的width动画过程中重心跑偏了！
             ValueAnimator squashAnim1 = ObjectAnimator.ofFloat(newBall, "x", newBall.getX(),
                     newBall.getX() - 25f);
             squashAnim1.setDuration(duration/4);
             squashAnim1.setRepeatCount(1);
             squashAnim1.setRepeatMode(ValueAnimator.REVERSE);
             squashAnim1.setInterpolator(new DecelerateInterpolator());
+            
+            //横向拉伸的动画
             ValueAnimator squashAnim2 = ObjectAnimator.ofFloat(newBall, "width", newBall.getWidth(),
                     newBall.getWidth() + 50);
             squashAnim2.setDuration(duration/4);
             squashAnim2.setRepeatCount(1);
             squashAnim2.setRepeatMode(ValueAnimator.REVERSE);
             squashAnim2.setInterpolator(new DecelerateInterpolator());
+            
+            //降低重心
             ValueAnimator stretchAnim1 = ObjectAnimator.ofFloat(newBall, "y", endY,
                     endY + 25f);
             stretchAnim1.setDuration(duration/4);
             stretchAnim1.setRepeatCount(1);
             stretchAnim1.setInterpolator(new DecelerateInterpolator());
             stretchAnim1.setRepeatMode(ValueAnimator.REVERSE);
+            
+            //纵向压扁的动画
             ValueAnimator stretchAnim2 = ObjectAnimator.ofFloat(newBall, "height",
                     newBall.getHeight(), newBall.getHeight() - 25);
             stretchAnim2.setDuration(duration/4);
@@ -121,6 +151,8 @@ public class BouncingBalls extends Activity {
             bounceBackAnim.setDuration(duration);
             bounceBackAnim.setInterpolator(new DecelerateInterpolator());
             // Sequence the down/squash&stretch/up animations
+            
+            //这没啥好说的了，这代码跟读文章一样
             AnimatorSet bouncer = new AnimatorSet();
             bouncer.play(bounceAnim).before(squashAnim1);
             bouncer.play(squashAnim1).with(squashAnim2);
@@ -128,6 +160,7 @@ public class BouncingBalls extends Activity {
             bouncer.play(squashAnim1).with(stretchAnim2);
             bouncer.play(bounceBackAnim).after(stretchAnim2);
 
+            //完事儿，淡出
             // Fading animation - remove the ball when the animation is done
             ValueAnimator fadeAnim = ObjectAnimator.ofFloat(newBall, "alpha", 1f, 0f);
             fadeAnim.setDuration(250);
@@ -149,9 +182,17 @@ public class BouncingBalls extends Activity {
             return true;
         }
 
+        /**
+         * 
+         * @param x 初始化坐标x
+         * @param y 初始化坐标y
+         * @return
+         */
         private ShapeHolder addBall(float x, float y) {
+        	//这个跟布局文件里面创建的shape是一回事
             OvalShape circle = new OvalShape();
             circle.resize(50f, 50f);
+            //封装进drawable
             ShapeDrawable drawable = new ShapeDrawable(circle);
             ShapeHolder shapeHolder = new ShapeHolder(drawable);
             shapeHolder.setX(x - 25f);
@@ -164,6 +205,7 @@ public class BouncingBalls extends Activity {
             int darkColor = 0xff000000 | red/4 << 16 | green/4 << 8 | blue/4;
             RadialGradient gradient = new RadialGradient(37.5f, 12.5f,
                     50f, color, darkColor, Shader.TileMode.CLAMP);
+            //加了个遮罩，仔细观察生成的球，右上角有高光反射点
             paint.setShader(gradient);
             shapeHolder.setPaint(paint);
             balls.add(shapeHolder);
@@ -174,6 +216,7 @@ public class BouncingBalls extends Activity {
         protected void onDraw(Canvas canvas) {
             for (int i = 0; i < balls.size(); ++i) {
                 ShapeHolder shapeHolder = balls.get(i);
+                //因为事件触发后我们要绘制多个（balls是个list嘛）ball，所以如果不save、restore的话，相互之间的矩阵变换就会有影响。
                 canvas.save();
                 canvas.translate(shapeHolder.getX(), shapeHolder.getY());
                 shapeHolder.getShape().draw(canvas);
